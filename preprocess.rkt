00934082403393a8a241e5c83e7a53ac686287f3
Nuno Santos <nuno_913@hotmail.com>
2018-05-05 18:07:11 +0100

    Pre-processor part done

--- preprocess.rkt
+++ preprocess.rkt
@@ -1,8 +1,9 @@
 #lang racket
 (provide add-active-token def-active-token process-string)
+(require dyoo-while-loop)
 
 ;List that saves the tokens that were defined
-;(now only stores the functions, should store the pairs <token,function>)
+;stores the pairs <token,function>
 (define tokens '())
 
 
@@ -10,9 +11,9 @@
 takes a string describing an active token and a function that should be triggered when that token is found.
 This last function takes a string and returns a string.
 This function stores the association between the token and the corresponding function.|#
-(define (add-active-token str f)
-  (set! tokens (append tokens (list f)))   ;stores the function
-  (displayln tokens))
+(define (add-active-token token f)
+  (set! tokens (append tokens (list (list token f)))))   ;stores the function
+  
 
 
 #|process-string
@@ -21,8 +22,26 @@ transforming the string until no more active tokens are found.
 To this end, whenever an active token is found, the associated function is called with the substring that starts immediately after the token,
 and the result of that function replaces the substring that starts with the token.|#
 (define (process-string str)
-  (for ([token-saved tokens])
-    (displayln (token-saved str))))      ;should do a for loop for each token that was defined, then an if or something to try to apply it
+  (for ([entry tokens])                                                   ;for each defined token
+    (let ([token (car entry)]
+          [f (cadr entry)])
+      (define token-match (regexp-match-positions token str))             ;tries to find the token in the string
+      (while (not (equal? token-match #f))                                ;while there is a match
+             (set! str (transform-string str f token-match))              ;transforms the string
+             (set! token-match (regexp-match-positions token str)))))     ;tries to find match in new string
+  str)
+
+
+;with the token match in the string, calls the function f on the string after the token,
+;then appends the string before the token with the result of the function
+(define (transform-string str f token-match)
+  (define pos-token (caar token-match))
+  (define pos-after-token (cdar token-match))
+  (define pre-string (substring str 0 pos-token))
+  (define pos-string (substring str pos-after-token))
+  (define result-pos-string (f pos-string))
+  (string-append pre-string result-pos-string)
+)
 
 
 #|def-active-token
@@ -42,12 +61,32 @@ expands into an appropriate use of the function add-active-token.|#
              (substring str (+ i 1))))
       ""))
 
-#|(define ns (make-base-namespace))
+(define ns (make-base-namespace))
 (def-active-token "//eval " (str)
   (call-with-input-string
    str
    (lambda (in)
      (string-append (~a (eval (read in) ns))
-                    (port->string in)))))|#
+                    (port->string in)))))
 
 
+
+;tokens that we need to define
+
+;Local Type Inference
+(def-active-token "var" (str)
+  ;do stuff
+  "a"
+  )
+
+;String Interpolation
+(def-active-token "#" (str)
+  ;do stuff
+  "b"
+  )
+
+;Type Aliases
+(def-active-token "alias" (str)
+  ;do stuff
+  "c"
+  )
